d892c2dccb8880ddd21a5d4d742a0e57c45b3e24
hzla00 <andylee.hzl@gmail.com>
2014-11-27 01:25:14 -0800

    put back create action

--- app/controllers/simple/events_controller.rb
+++ app/controllers/simple/events_controller.rb
@@ -8,6 +8,16 @@ class Simple::EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    p params["questions"]
+    puts "\n" * 10
+    if params["questions"] != ","
+      p "hid"
+      Event.create_simple_event params, current_user
+    end
+    redirect_to dashboard_path
+  end
+
   def index
   end
 
