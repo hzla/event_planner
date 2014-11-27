3a63c4eefd6d6b476ac0e7f071edd68e22251e36
hzla00 <andylee.hzl@gmail.com>
2014-11-27 02:36:48 -0800

    added redirect for vote submit

--- app/controllers/simple/polls_controller.rb
+++ app/controllers/simple/polls_controller.rb
@@ -6,7 +6,7 @@ class Simple::PollsController < ApplicationController
   def vote
     @poll = Poll.find params[:id]
     @poll.vote_with params[:choice_values]
-    render nothing: true
+    redirect_to simple_results_path(id: @poll.event.id)
   end
 
 end
