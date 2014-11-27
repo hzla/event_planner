b4f10c659858dc1d7ef5467ad433d3281cf39222
hzla00 <andylee.hzl@gmail.com>
2014-11-27 01:43:47 -0800

    added results link and redirect to correct poll page

--- app/controllers/simple/events_controller.rb
+++ app/controllers/simple/events_controller.rb
@@ -9,10 +9,7 @@ class Simple::EventsController < ApplicationController
   end
 
   def create
-    p params["questions"]
-    puts "\n" * 10
     if params["questions"] != ","
-      p "hid"
       Event.create_simple_event params, current_user
     end
     redirect_to dashboard_path
